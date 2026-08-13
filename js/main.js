2ed44e1213f851a3738e842e716e2560e3ae73d4
Toofan Javanshir <mydigiexperts@gmail.com>
2026-08-13 23:34:55 +1000

    Fix Apps Script URL suffix: /exec not /e

--- js/main.js
+++ js/main.js
@@ -100,7 +100,7 @@ document.getElementById('contactForm')?.addEventListener('submit', async functio
   };
 
   try {
-    await fetch('https://script.google.com/macros/s/AKfycbxOC-tTHq4zNrvynTE9ZJRn6vTDzjW2vscble4FTWTrc1EtT2pyHnq-P9bCsikIrfoF/e', {
+    await fetch('https://script.google.com/macros/s/AKfycbxOC-tTHq4zNrvynTE9ZJRn6vTDzjW2vscble4FTWTrc1EtT2pyHnq-P9bCsikIrfoF/exec', {
       method: 'POST',
       mode: 'no-cors',
       headers: { 'Content-Type': 'text/plain;charset=utf-8' },
